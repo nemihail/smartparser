718c192aa4aeeaa4aaae2ca7b32ca1267b4466c0
nemihail <nemihail02@gmail.com>
2026-04-15 13:32:58 +0700

    simple parser version
    it only prints data of every book like price, name, rating. nothing more

---
+++ simple-version.py
@@ -0,0 +1,22 @@
+
+import requests as rq
+from bs4 import BeautifulSoup
+
+url_wb = 'https://books.toscrape.com/'
+
+request = rq.get(url=url_wb)
+
+if request.ok:
+    html = request.text
+    soup = BeautifulSoup(html, 'html.parser')
+    books = soup.find_all('article', class_='product_pod')
+    for book in books:
+        title = book.h3.a['title']
+        price = book.find('p', class_='price_color').text
+        rating = book.p['class'][1]
+        print(f'Book title: {title}, book price: {price}, book rating: {rating}')
+else:
+    print('error in request')
+
+
+# print(books)
