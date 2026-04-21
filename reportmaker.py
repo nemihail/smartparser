e573e1b79a262016a746f0c0ad9378db25aac545
nemihail <nemihail02@gmail.com>
2026-04-21 22:01:03 +0700

    it barely can put inputted book in .csv file
    soreh for bed eengleesh

--- reportmaker.py
+++ reportmaker.py
@@ -1,12 +1,32 @@
 
-import pandas as pdd
 from pathlib import Path
-import json
+import csv
+from datetime import date
+
+reports = []
 
 try:
-    reportsdir = Path('reports')
+    reportsdir = Path('smartparser/reports')
     for report in reportsdir.iterdir():
         if report.suffix.lower() == '.csv':
-            print(report.name)
+            reports.append(report.name)
 except FileNotFoundError:
     Path('smartparser/reports').mkdir(parents=True, exist_ok=True)
+
+with open(f'smartparser/reports/{input('Report name: ')}.csv', mode='r', newline='', encoding='utf-8') as file:
+    data = [
+        ['Name', 'Price', 'Category', 'Date']
+    ]
+    reader = csv.reader(file)
+    for row in reader:
+        data.append(row)
+    
+
+data.append([input('Book name: '),
+             float(input('Book price, £: ')),
+             input('Book category aka genre: '),
+             date.today()])
+
+with open(f'smartparser/reports/{input('Report name: ')}.csv', mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
